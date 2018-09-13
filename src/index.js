8de854ebffc83cf500eda5bf0e48b8403a59c1fc
Dongor7 <dongor7@gmail.com>
2018-09-13 12:28:18 +0300

    Added pseudo matrix

--- src/index.js
+++ src/index.js
@@ -1,34 +1,56 @@
 function solveSudoku(matrix) {
 
     let data = {
-        unknownValuesCount: 1,
-        matrix: matrix,
+        unknownValuesCount: 0,
+        previousUnknownValuesCount: 0,
+        matrix,
+        pseudoMatrix: [],
         maxCount: 10,
-        currentCount: 0
+        currentCount: 0,
+        isLoop: false,
     };
 
+    /*for (let i = 0; i < matrix.length; i++) {
+        data.pseudoMatrix[i] = new Array(9);
+        for (let j = 0; j < matrix.length; j++) {
+            data.pseudoMatrix[i][j] = 0;
+        }
+    }*/
+
     do {
-        Object.assign(data,findZeros(data.matrix));
+        Object.assign(data,findZeros(data));
         data.currentCount++;
-    } while (data.unknownValuesCount > 0 && data.currentCount < data.maxCount);
+
+    } while (data.unknownValuesCount > 0 && data.currentCount < 10);
+
+
+    console.log(data.pseudoMatrix);
 
     return matrix;
 }
 
-function findZeros(matrix) {
+function findZeros(data) {
 
-    let data = {
-        unknownValuesCount: 0
-    };
+    data.unknownValuesCount = 0;
 
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix.length; j++) {
+    for (let i = 0; i < data.matrix.length; i++) {
+        for (let j = 0; j < data.matrix.length; j++) {
 
-            if (matrix[i][j] === 0) {
-                let hackedValue = hackValue(matrix, i, j);
-                if (hackedValue === 0)
+            if (data.isLoop) {
+                data.pseudoMatrix[i][j] = hackMagicValue(data, i, j);
+                data.unknownValuesCount++;
+            }
+
+            if (data.matrix[i][j] === 0) {
+                let hackedValue = hackValue(data.matrix, i, j);
+                if (hackedValue instanceof Array) {
                     data.unknownValuesCount++;
-                matrix[i][j] = hackedValue;
+                    data.matrix[i][j] = 0;
+                    //data.pseudoMatrix[i][j] = hackedValue;
+                } else {
+                    data.matrix[i][j] = hackedValue;
+                }
+
             }
 
         }
@@ -36,7 +58,7 @@ function findZeros(matrix) {
 
     console.log(data.unknownValuesCount);
 
-    data.matrix = matrix;
+    setIsLoop(data);
 
     return data;
 }
@@ -63,15 +85,64 @@ function hackValue(matrix, y, x) {
     }
 
     let uniqueExistsNumbers = [...new Set(existsNumbers)];
-
     let baseNumberConsequence = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    let filteredConsequence = baseNumberConsequence.filter(function(obj) {
-        return uniqueExistsNumbers.indexOf(obj) === -1;
+    let filteredConsequence = baseNumberConsequence.filter(item => {
+        return uniqueExistsNumbers.indexOf(item) === -1;
     });
 
-    return filteredConsequence.length === 1 ? filteredConsequence[0] : 0;
+    return filteredConsequence.length === 1 ? filteredConsequence[0] : filteredConsequence;
+
 }
+
+function hackMagicValue(data, y, x) {
+
+    for (let i = 0; i < data.pseudoMatrix.length; i++) {
+        for (let j = 0; j < data.pseudoMatrix.length; j++) {
+            /*if (data.pseudoMatrix[i][j] !== 0) {
+                data.pseudoMatrix[i][j] = data.pseudoMatrix[i][j];
+            }*/
+
+            //data.pseudoMatrix[y][x] = data.pseudoMatrix[y][x]
+        }
+    }
+
+    return data.pseudoMatrix[y][x];
+
+}
+
+function setIsLoop(data) {
+    if (data.previousUnknownValuesCount === data.unknownValuesCount) {
+        data.isLoop = true;
+        initializeWithMagicValues(data);
+    }
+    else {
+        data.previousUnknownValuesCount = data.unknownValuesCount;
+        data.isLoop = false;
+    }
+}
+
+function initializeWithMagicValues(data) {
+
+    data.pseudoMatrix = data.matrix.slice();
+
+    for (let i = 0; i < data.matrix.length; i++) {
+        data.pseudoMatrix[i] = data.matrix[i].slice();
+        for (let j = 0; j < data.matrix.length; j++) {
+            data.pseudoMatrix[i][j] = data.matrix[i][j];
+        }
+    }
+
+    for (let i = 0; i < data.pseudoMatrix.length; i++) {
+        for (let j = 0; j < data.pseudoMatrix.length; j++) {
+
+            if (data.pseudoMatrix[i][j] === 0) {
+                data.pseudoMatrix[i][j] = hackValue(data.pseudoMatrix, i, j);
+            }
+
+        }
+    }
+}
+
 
 const initial = [
     [6, 5, 0, 7, 3, 0, 0, 8, 0],
@@ -85,6 +156,6 @@ const initial = [
     [1, 6, 5, 3, 9, 0, 4, 7, 0]
 ];
 
-console.log(solveSudoku(initial));;
+console.log(solveSudoku(initial));
 
 module.exports = solveSudoku;
