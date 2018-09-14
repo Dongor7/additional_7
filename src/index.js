f0f2288d42f413a2c8ebedd7afc015055abc5359
Denis Sidorenko <dongor7@gmail.com>
2018-09-14 14:41:38 +0300

    second test completed

--- src/index.js
+++ src/index.js
@@ -17,50 +17,28 @@ function solveSudoku(matrix) {
     if(!data.isLoop)
         return matrix;
 
-    console.log(matrix);
-    console.log();
-
     findZeros(data, true);
 
-    console.log(data.unknownPositions);
-    console.log();
-
-    data.deep = 0;
-
-    addArrayLevel(data);
-
-    return matrix;
-}
-
-function addArrayLevel(data) {
-
-    let element = data.unknownPositions[data.deep];
-
-    data.deep++;
-
-    for (let i = 0; i < element.values.length; i++) {
-
-        if (checkSudoku(data)) {
-            return
-        }
-
-        data.matrix[element.i][element.j] = element.values[i];
-
-        if (data.deep < data.unknownPositions.length) {
-            addArrayLevel(data);
-            data.deep--;
-        } else {
-            return;
-        }
+    while(data.unknownPositions.length > 0) {
+        //console.log(data.unknownPositions);
+        //console.log();
+        compareSets(data);
+        console.log(data.unknownPositions);
+        console.log();
+        findZeros(data, true);
+        console.log(data.unknownPositions);
+        console.log();
     }
 
-
-
+    return matrix;
 }
 
 function findZeros(data, isFinal) {
 
     data.unknownValuesCount = 0;
+
+    if(isFinal)
+        data.unknownPositions = [];
 
     for (let i = 0; i < data.matrix.length; i++) {
         for (let j = 0; j < data.matrix.length; j++) {
@@ -119,15 +97,12 @@ function hackValue(matrix, y, x) {
 
     let existsNumbers = [];
 
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix.length; j++) {
+    for (let j = 0; j < matrix.length; j++) {
 
-            if (!(matrix[y][j] instanceof Array))
-                existsNumbers.push(matrix[y][j]);
-            if (!(matrix[j][x] instanceof Array))
-                existsNumbers.push(matrix[j][x]);
-        }
-        break;
+        if (!(matrix[y][j] instanceof Array))
+            existsNumbers.push(matrix[y][j]);
+        if (!(matrix[j][x] instanceof Array))
+            existsNumbers.push(matrix[j][x]);
     }
 
     let YCell = ~~(y / 3);
@@ -150,6 +125,99 @@ function hackValue(matrix, y, x) {
 
 }
 
+function compareSets(data) {
+
+    startLoopToCompareTwo(data);
+    startLoopToCompareThree(data);
+
+}
+
+function startLoopToCompareThree(data) {
+
+
+
+}
+
+function startLoopToCompareTwo(data) {
+    for (let i = 0; i < data.unknownPositions.length - 2; i++){
+
+        if (data.unknownPositions[i].i === data.unknownPositions[i + 1].i && data.unknownPositions[i + 1].i === data.unknownPositions[i + 2].i) {
+            if (data.unknownPositions[i].values.sort().toString() === data.unknownPositions[i + 1].values.sort().toString()
+                && data.unknownPositions[i + 1].values.length === 2) {
+                data.unknownPositions[i + 2].values = data.unknownPositions[i + 2].values.filter(item => {
+                    return data.unknownPositions[i + 1].values.indexOf(item) === -1;
+                });
+                if (data.unknownPositions[i + 2].values.length === 1) {
+                    let elem = data.unknownPositions[i + 2];
+                    data.matrix[elem.i][elem.j] = elem.values[0];
+                }
+            }
+            if (data.unknownPositions[i].values.sort().toString() === data.unknownPositions[i + 2].values.sort().toString()
+                && data.unknownPositions[i + 2].values.length === 2) {
+                data.unknownPositions[i + 1].values = data.unknownPositions[i + 1].values.filter(item => {
+                    return data.unknownPositions[i + 2].values.indexOf(item) === -1;
+                });
+                if (data.unknownPositions[i + 1].values.length === 1) {
+                    let elem = data.unknownPositions[i + 1];
+                    data.matrix[elem.i][elem.j] = elem.values[0];
+                }
+            }
+            if (data.unknownPositions[i + 2].values.sort().toString() === data.unknownPositions[i + 1].values.sort().toString()
+                && data.unknownPositions[i + 1].values.length === 2) {
+                data.unknownPositions[i].values = data.unknownPositions[i].values.filter(item => {
+                    return data.unknownPositions[i + 1].values.indexOf(item) === -1;
+                });
+                if (data.unknownPositions[i].values.length === 1) {
+                    let elem = data.unknownPositions[i];
+                    data.matrix[elem.i][elem.j] = elem.values[0];
+                }
+            }
+        }
+
+    }
+
+    data.unknownPositions.sort((left, right) => left.j - right.j);
+
+    for (let i = 0; i < data.unknownPositions.length - 2; i++){
+
+        if (data.unknownPositions[i].j === data.unknownPositions[i + 1].j && data.unknownPositions[i + 1].j === data.unknownPositions[i + 2].j) {
+            if(data.unknownPositions[i].values.sort().toString() === data.unknownPositions[i + 1].values.sort().toString()
+                && data.unknownPositions[i + 1].values.length === 2) {
+                data.unknownPositions[i + 2].values = data.unknownPositions[i + 2].values.filter(item => {
+                    return data.unknownPositions[i + 1].values.indexOf(item) === -1;
+                });
+                if (data.unknownPositions[i + 2].values.length === 1) {
+                    let elem = data.unknownPositions[i + 2];
+                    data.matrix[elem.i][elem.j] = elem.values[0];
+                }
+            }
+            if(data.unknownPositions[i].values.sort().toString() === data.unknownPositions[i + 2].values.sort().toString()
+                && data.unknownPositions[i + 2].values.length === 2) {
+                data.unknownPositions[i + 1].values = data.unknownPositions[i + 1].values.filter(item => {
+                    return data.unknownPositions[i + 2].values.indexOf(item) === -1;
+                });
+                if (data.unknownPositions[i + 1].values.length === 1) {
+                    let elem = data.unknownPositions[i + 1];
+                    data.matrix[elem.i][elem.j] = elem.values[0];
+                }
+            }
+            if(data.unknownPositions[i + 2].values.sort().toString() === data.unknownPositions[i + 1].values.sort().toString()
+                && data.unknownPositions[i + 1].values.length === 2) {
+                data.unknownPositions[i].values = data.unknownPositions[i].values.filter(item => {
+                    return data.unknownPositions[i + 1].values.indexOf(item) === -1;
+                });
+                if (data.unknownPositions[i].values.length === 1) {
+                    let elem = data.unknownPositions[i];
+                    data.matrix[elem.i][elem.j] = elem.values[0];
+                }
+            }
+        }
+
+    }
+
+    data.unknownPositions.sort((left, right) => left.i - right.i);
+}
+
 function setIsLoop(data) {
     if (data.previousUnknownValuesCount === data.unknownValuesCount) {
         data.isLoop = true;
@@ -161,15 +229,15 @@ function setIsLoop(data) {
 }
 
 const initial = [
-    [6, 5, 0, 7, 3, 0, 0, 8, 0],
-    [0, 0, 0, 4, 8, 0, 5, 3, 0],
-    [8, 4, 0, 9, 2, 5, 0, 0, 0],
-    [0, 9, 0, 8, 0, 0, 0, 0, 0],
-    [5, 3, 0, 2, 0, 9, 6, 0, 0],
-    [0, 0, 6, 0, 0, 0, 8, 0, 0],
-    [0, 0, 9, 0, 0, 0, 0, 0, 6],
-    [0, 0, 7, 0, 0, 0, 0, 5, 0],
-    [1, 6, 5, 3, 9, 0, 4, 7, 0]
+    [0, 5, 0, 0, 7, 0, 0, 0, 1],
+    [8, 7, 6, 0, 2, 1, 9, 0, 3],
+    [0, 0, 0, 0, 3, 5, 0, 0, 0],
+    [0, 0, 0, 0, 4, 3, 6, 1, 0],
+    [0, 4, 0, 0, 0, 9, 0, 0, 2],
+    [0, 1, 2, 0, 5, 0, 0, 0, 4],
+    [0, 8, 9, 0, 6, 4, 0, 0, 0],
+    [0, 0, 0, 0, 0, 7, 0, 0, 0],
+    [1, 6, 7, 0, 0, 2, 5, 4, 0]
 ];
 
 console.log(solveSudoku(initial));
