f19e86b92215753388dabf2d0208ec801d0a2c52
Dongor7 <dongor7@gmail.com>
2018-09-18 23:03:05 +0300

    Added some value for magicThree

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ function solveSudoku(matrix) {
 
     findZeros(data, true);
 
+    console.log(data.unknownPositions);
+    console.log(data.unknownPositions.length);
+    console.log();
+
     while(data.unknownPositions.length > 0) {
         //console.log(data.unknownPositions);
         //console.log();
@@ -35,6 +39,8 @@ function solveSudoku(matrix) {
         console.log(data.unknownPositions);
         console.log();
     }
+
+    console.log(data.unknownPositions.length);
 
     return matrix;
 }
@@ -138,6 +144,94 @@ function compareSets(data) {
 
 }
 
+function startLoopToCompareTwo(data) {
+
+    for (let i = 0; i < data.unknownPositions.length - 2; i++){
+        for (let j = 1 + i; j < data.unknownPositions.length - 1; j++){
+            for (let k = 1 + j; k < data.unknownPositions.length; k++){
+                if (data.unknownPositions[i].i === data.unknownPositions[j].i && data.unknownPositions[j].i === data.unknownPositions[k].i) {
+                    if (data.unknownPositions[i].values.sort().toString() === data.unknownPositions[j].values.sort().toString()
+                        && data.unknownPositions[j].values.length === 2) {
+                        data.unknownPositions[k].values = data.unknownPositions[k].values.filter(item => {
+                            return data.unknownPositions[j].values.indexOf(item) === -1;
+                        });
+                        if (data.unknownPositions[k].values.length === 1) {
+                            let elem = data.unknownPositions[k];
+                            data.matrix[elem.i][elem.j] = elem.values[0];
+                        }
+                    }
+                    if (data.unknownPositions[i].values.sort().toString() === data.unknownPositions[k].values.sort().toString()
+                        && data.unknownPositions[k].values.length === 2) {
+                        data.unknownPositions[j].values = data.unknownPositions[j].values.filter(item => {
+                            return data.unknownPositions[k].values.indexOf(item) === -1;
+                        });
+                        if (data.unknownPositions[j].values.length === 1) {
+                            let elem = data.unknownPositions[j];
+                            data.matrix[elem.i][elem.j] = elem.values[0];
+                        }
+                    }
+                    if (data.unknownPositions[k].values.sort().toString() === data.unknownPositions[j].values.sort().toString()
+                        && data.unknownPositions[j].values.length === 2) {
+                        data.unknownPositions[i].values = data.unknownPositions[i].values.filter(item => {
+                            return data.unknownPositions[j].values.indexOf(item) === -1;
+                        });
+                        if (data.unknownPositions[i].values.length === 1) {
+                            let elem = data.unknownPositions[i];
+                            data.matrix[elem.i][elem.j] = elem.values[0];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    data.unknownPositions.sort((left, right) => left.j - right.j);
+
+    for (let i = 0; i < data.unknownPositions.length - 2; i++){
+        for (let j = 1 + i; j < data.unknownPositions.length - 1; j++){
+            for (let k = 1 + j; k < data.unknownPositions.length; k++) {
+                if (data.unknownPositions[i].j === data.unknownPositions[j].j && data.unknownPositions[j].j === data.unknownPositions[k].j) {
+
+                    if (data.unknownPositions[i].values.sort().toString() === data.unknownPositions[j].values.sort().toString()
+                        && data.unknownPositions[j].values.length === 2) {
+                        data.unknownPositions[k].values = data.unknownPositions[k].values.filter(item => {
+                            return data.unknownPositions[j].values.indexOf(item) === -1;
+                        });
+                        if (data.unknownPositions[k].values.length === 1) {
+                            let elem = data.unknownPositions[k];
+                            data.matrix[elem.i][elem.j] = elem.values[0];
+                        }
+                    }
+                    if (data.unknownPositions[i].values.sort().toString() === data.unknownPositions[k].values.sort().toString()
+                        && data.unknownPositions[k].values.length === 2) {
+                        data.unknownPositions[j].values = data.unknownPositions[j].values.filter(item => {
+                            return data.unknownPositions[k].values.indexOf(item) === -1;
+                        });
+                        if (data.unknownPositions[j].values.length === 1) {
+                            let elem = data.unknownPositions[j];
+                            data.matrix[elem.i][elem.j] = elem.values[0];
+                        }
+                    }
+                    if (data.unknownPositions[k].values.sort().toString() === data.unknownPositions[j].values.sort().toString()
+                        && data.unknownPositions[j].values.length === 2) {
+                        data.unknownPositions[i].values = data.unknownPositions[i].values.filter(item => {
+                            return data.unknownPositions[j].values.indexOf(item) === -1;
+                        });
+                        if (data.unknownPositions[i].values.length === 1) {
+                            let elem = data.unknownPositions[i];
+                            data.matrix[elem.i][elem.j] = elem.values[0];
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    data.unknownPositions.sort((left, right) => left.i - right.i);
+
+}
+
 function startLoopToCompareThree(data) {
 
     /* First line */
@@ -147,33 +241,92 @@ function startLoopToCompareThree(data) {
     for (let i = 0 * 3; i < (0 + 1) * 3; i++) {
         for (let j = 0 * 3 ; j < (0 + 1) * 3; j++) {
             for (let k = 0; k < data.unknownPositions.length; k++) {
-                if (data.unknownPositions[k].i === i && data.unknownPositions[k].j === j) {
+                if (data.unknownPositions[k].i === i && data.unknownPositions[k].j === j && data.unknownPositions[k].values.length < 4) {
                     positions.push(data.unknownPositions[k]);
                 }
             }
         }
+    }
 
-        if (positions.length >= 3) {
+    console.log('Positions: 1 = ', positions);
+    console.log();
 
+    if (positions.length >= 3) {
+        
+        for(let i = 0; i < positions.length - 2; i++) {
+            for(let j = 1 + i; j < positions.length - 1; j++) {
+                for(let k = 1 + j; k < positions.length; k++) {
+                    
+                    let numbers = positions[i].values.concat(positions[j].values);
+                    numbers = numbers.concat(positions[k].values);
+                    let uniqueNumbers= [...new Set(numbers)];
+
+                    if (uniqueNumbers.length === 3) {
+
+                        for (let n = 0 * 3; n < (0 + 1) * 3; n++) {
+                            for (let m = 0 * 3; m < (0 + 1) * 3; m++) {
+                                for (let b = 0; b < data.unknownPositions.length; b++) {
+                                    if (data.unknownPositions[b].i === n && data.unknownPositions[b].j === m) {
+
+                                        if (!(data.unknownPositions[b].i === positions[i].i && data.unknownPositions[b].j === positions[i].j)){
+                                            if (!(data.unknownPositions[b].i === positions[j].i && data.unknownPositions[b].j === positions[j].j)) {
+                                                if (!(data.unknownPositions[b].i === positions[k].i && data.unknownPositions[b].j === positions[k].j)) {
+
+                                                    data.unknownPositions[b].values = data.unknownPositions[b].values.filter(item => {
+                                                        return uniqueNumbers.indexOf(item) === -1;
+                                                    });
+
+                                                    if (data.unknownPositions[b].values.length === 1) {
+                                                        let elem = data.unknownPositions[b];
+                                                        data.matrix[elem.i][elem.j] = elem.values[0];
+                                                    }
+
+                                                }
+                                            }
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                    
+                }
+            }
         }
-
+        
     }
 
     positions = [];
 
     for (let i = 0 * 3; i < (0 + 1) * 3; i++) {
         for (let j = 1 * 3 ; j < (1 + 1) * 3; j++) {
-
+            for (let k = 0; k < data.unknownPositions.length; k++) {
+                if (data.unknownPositions[k].i === i && data.unknownPositions[k].j === j) {
+                    positions.push(data.unknownPositions[k]);
+                }
+            }
         }
     }
+
+    console.log('Positions: 2 = ', positions);
+    console.log();
 
     positions = [];
 
     for (let i = 0 * 3; i < (0 + 1) * 3; i++) {
         for (let j = 2 * 3 ; j < (2 + 1) * 3; j++) {
-
+            for (let k = 0; k < data.unknownPositions.length; k++) {
+                if (data.unknownPositions[k].i === i && data.unknownPositions[k].j === j) {
+                    positions.push(data.unknownPositions[k]);
+                }
+            }
         }
     }
+
+    console.log('Positions: 3 = ', positions);
+    console.log();
 
     /* Second line */
 
@@ -181,24 +334,92 @@ function startLoopToCompareThree(data) {
 
     for (let i = 1 * 3; i < (1 + 1) * 3; i++) {
         for (let j = 0 * 3 ; j < (0 + 1) * 3; j++) {
-
+            for (let k = 0; k < data.unknownPositions.length; k++) {
+                if (data.unknownPositions[k].i === i && data.unknownPositions[k].j === j) {
+                    positions.push(data.unknownPositions[k]);
+                }
+            }
         }
     }
+
+    console.log('Positions: 4 = ', positions);
+    console.log();
 
     positions = [];
 
     for (let i = 1 * 3; i < (1 + 1) * 3; i++) {
         for (let j = 1 * 3 ; j < (1 + 1) * 3; j++) {
-
+            for (let k = 0; k < data.unknownPositions.length; k++) {
+                if (data.unknownPositions[k].i === i && data.unknownPositions[k].j === j) {
+                    positions.push(data.unknownPositions[k]);
+                }
+            }
         }
     }
+
+    console.log('Positions: 5 = ', positions);
+    console.log();
 
     positions = [];
 
     for (let i = 1 * 3; i < (1 + 1) * 3; i++) {
         for (let j = 2 * 3 ; j < (2 + 1) * 3; j++) {
-
+            for (let k = 0; k < data.unknownPositions.length; k++) {
+                if (data.unknownPositions[k].i === i && data.unknownPositions[k].j === j) {
+                    positions.push(data.unknownPositions[k]);
+                }
+            }
         }
+    }
+
+    console.log('Positions: 6 = ', positions);
+    console.log();
+
+    if (positions.length >= 3) {
+
+        for(let i = 0; i < positions.length - 2; i++) {
+            for(let j = 1 + i; j < positions.length - 1; j++) {
+                for(let k = 1 + j; k < positions.length; k++) {
+
+                    let numbers = positions[i].values.concat(positions[j].values);
+                    numbers = numbers.concat(positions[k].values);
+                    let uniqueNumbers= [...new Set(numbers)];
+
+                    if (uniqueNumbers.length === 3) {
+
+                        for (let n = 1 * 3; n < (1 + 1) * 3; n++) {
+                            for (let m = 2 * 3; m < (2 + 1) * 3; m++) {
+                                for (let b = 0; b < data.unknownPositions.length; b++) {
+                                    if (data.unknownPositions[b].i === n && data.unknownPositions[b].j === m) {
+
+                                        if (!(data.unknownPositions[b].i === positions[i].i && data.unknownPositions[b].j === positions[i].j)){
+                                            if (!(data.unknownPositions[b].i === positions[j].i && data.unknownPositions[b].j === positions[j].j)) {
+                                                if (!(data.unknownPositions[b].i === positions[k].i && data.unknownPositions[b].j === positions[k].j)) {
+
+                                                    data.unknownPositions[b].values = data.unknownPositions[b].values.filter(item => {
+                                                        return uniqueNumbers.indexOf(item) === -1;
+                                                    });
+
+                                                    if (data.unknownPositions[b].values.length === 1) {
+                                                        let elem = data.unknownPositions[b];
+                                                        data.matrix[elem.i][elem.j] = elem.values[0];
+                                                    }
+
+                                                }
+                                            }
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        }
+
     }
 
     /* Third line */
@@ -207,105 +428,46 @@ function startLoopToCompareThree(data) {
 
     for (let i = 2 * 3; i < (2 + 1) * 3; i++) {
         for (let j = 0 * 3 ; j < (0 + 1) * 3; j++) {
-
+            for (let k = 0; k < data.unknownPositions.length; k++) {
+                if (data.unknownPositions[k].i === i && data.unknownPositions[k].j === j) {
+                    positions.push(data.unknownPositions[k]);
+                }
+            }
         }
     }
+
+    console.log('Positions: 7 = ', positions);
+    console.log();
 
     positions = [];
 
     for (let i = 2 * 3; i < (2 + 1) * 3; i++) {
         for (let j = 1 * 3 ; j < (1 + 1) * 3; j++) {
-
+            for (let k = 0; k < data.unknownPositions.length; k++) {
+                if (data.unknownPositions[k].i === i && data.unknownPositions[k].j === j) {
+                    positions.push(data.unknownPositions[k]);
+                }
+            }
         }
     }
+
+    console.log('Positions: 8 = ', positions);
+    console.log();
 
     positions = [];
 
     for (let i = 2 * 3; i < (2 + 1) * 3; i++) {
         for (let j = 2 * 3 ; j < (2 + 1) * 3; j++) {
-
-        }
-    }
-}
-
-function startLoopToCompareTwo(data) {
-    for (let i = 0; i < data.unknownPositions.length - 2; i++){
-
-        if (data.unknownPositions[i].i === data.unknownPositions[i + 1].i && data.unknownPositions[i + 1].i === data.unknownPositions[i + 2].i) {
-            if (data.unknownPositions[i].values.sort().toString() === data.unknownPositions[i + 1].values.sort().toString()
-                && data.unknownPositions[i + 1].values.length === 2) {
-                data.unknownPositions[i + 2].values = data.unknownPositions[i + 2].values.filter(item => {
-                    return data.unknownPositions[i + 1].values.indexOf(item) === -1;
-                });
-                if (data.unknownPositions[i + 2].values.length === 1) {
-                    let elem = data.unknownPositions[i + 2];
-                    data.matrix[elem.i][elem.j] = elem.values[0];
-                }
-            }
-            if (data.unknownPositions[i].values.sort().toString() === data.unknownPositions[i + 2].values.sort().toString()
-                && data.unknownPositions[i + 2].values.length === 2) {
-                data.unknownPositions[i + 1].values = data.unknownPositions[i + 1].values.filter(item => {
-                    return data.unknownPositions[i + 2].values.indexOf(item) === -1;
-                });
-                if (data.unknownPositions[i + 1].values.length === 1) {
-                    let elem = data.unknownPositions[i + 1];
-                    data.matrix[elem.i][elem.j] = elem.values[0];
-                }
-            }
-            if (data.unknownPositions[i + 2].values.sort().toString() === data.unknownPositions[i + 1].values.sort().toString()
-                && data.unknownPositions[i + 1].values.length === 2) {
-                data.unknownPositions[i].values = data.unknownPositions[i].values.filter(item => {
-                    return data.unknownPositions[i + 1].values.indexOf(item) === -1;
-                });
-                if (data.unknownPositions[i].values.length === 1) {
-                    let elem = data.unknownPositions[i];
-                    data.matrix[elem.i][elem.j] = elem.values[0];
+            for (let k = 0; k < data.unknownPositions.length; k++) {
+                if (data.unknownPositions[k].i === i && data.unknownPositions[k].j === j) {
+                    positions.push(data.unknownPositions[k]);
                 }
             }
         }
-
     }
 
-    data.unknownPositions.sort((left, right) => left.j - right.j);
-
-    for (let i = 0; i < data.unknownPositions.length - 2; i++){
-
-        if (data.unknownPositions[i].j === data.unknownPositions[i + 1].j && data.unknownPositions[i + 1].j === data.unknownPositions[i + 2].j) {
-            if(data.unknownPositions[i].values.sort().toString() === data.unknownPositions[i + 1].values.sort().toString()
-                && data.unknownPositions[i + 1].values.length === 2) {
-                data.unknownPositions[i + 2].values = data.unknownPositions[i + 2].values.filter(item => {
-                    return data.unknownPositions[i + 1].values.indexOf(item) === -1;
-                });
-                if (data.unknownPositions[i + 2].values.length === 1) {
-                    let elem = data.unknownPositions[i + 2];
-                    data.matrix[elem.i][elem.j] = elem.values[0];
-                }
-            }
-            if(data.unknownPositions[i].values.sort().toString() === data.unknownPositions[i + 2].values.sort().toString()
-                && data.unknownPositions[i + 2].values.length === 2) {
-                data.unknownPositions[i + 1].values = data.unknownPositions[i + 1].values.filter(item => {
-                    return data.unknownPositions[i + 2].values.indexOf(item) === -1;
-                });
-                if (data.unknownPositions[i + 1].values.length === 1) {
-                    let elem = data.unknownPositions[i + 1];
-                    data.matrix[elem.i][elem.j] = elem.values[0];
-                }
-            }
-            if(data.unknownPositions[i + 2].values.sort().toString() === data.unknownPositions[i + 1].values.sort().toString()
-                && data.unknownPositions[i + 1].values.length === 2) {
-                data.unknownPositions[i].values = data.unknownPositions[i].values.filter(item => {
-                    return data.unknownPositions[i + 1].values.indexOf(item) === -1;
-                });
-                if (data.unknownPositions[i].values.length === 1) {
-                    let elem = data.unknownPositions[i];
-                    data.matrix[elem.i][elem.j] = elem.values[0];
-                }
-            }
-        }
-
-    }
-
-    data.unknownPositions.sort((left, right) => left.i - right.i);
+    console.log('Positions: 9 = ', positions);
+    console.log();
 }
 
 function setIsLoop(data) {
